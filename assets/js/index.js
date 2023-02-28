05d90075ec6e9455c4080e5497a6d740906fb71e
Jawad-Ahmindache <101027749+Jawad-Ahmindache@users.noreply.github.com>
2023-02-28 18:56:49 +0100

    Fusion terminée
    Pour éviter de casser le travail d'houmam et mon travail j'ai supprimé toutes mes classes css pour ne garder que les @mixins et j'ai mis toutes les classes css du main.
    
    Cette branche est déjà prête à être placer dans le main

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,9 @@
-//copilot
+let close = document.getElementById("header_nav_close");
+let toggle = document.getElementById("header_toggle");
+close.addEventListener("click", function () {
+    document.querySelector(".header_nav").classList.remove("active");
+});
+
+toggle.addEventListener("click", function () {
+    document.querySelector(".header_nav").classList.add("active");
+});
