8f75eaad4830479e2ae91cf5a893a8578989eae1
Jawad-Ahmindache <101027749+Jawad-Ahmindache@users.noreply.github.com>
2023-03-01 16:54:49 +0100

    navbar + autres ajouts

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,18 @@ close.addEventListener("click", function () {
 toggle.addEventListener("click", function () {
     document.querySelector(".header_nav").classList.add("active");
 });
+
+
+$(document).ready(function(){
+    // Changer page
+    $(".header_nav_list_item_link").click(function(){
+        let link = $(this).attr("data-link");
+        
+        let content = $(document.body).find('.content-box');
+
+        content.html('<p class = "text-light font-bold p-5 text-center">Chargement de '+ $(this).text() +'...</p>');
+        content.load("/html/"+link+".html");
+
+    });
+
+});
