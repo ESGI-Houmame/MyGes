f91e25c50e96a5c9e7bcf3cf774772c1ce0cdf67
DeltaCorporate <houms937@gmail.com>
2023-03-04 09:27:53 +0100

    :sparkles: home page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,41 @@
-    let close = document.getElementById("header_nav_close");
-    let toggle = document.getElementById("header_toggle");
-    close.addEventListener("click", function () {
-        document.querySelector(".header_nav").classList.remove("active");
-    });
+let close = document.getElementById("header_nav_close");
+let toggle = document.getElementById("header_toggle");
+close.addEventListener("click", function () {
+    document.querySelector(".header_nav").classList.remove("active");
+});
 
-    toggle.addEventListener("click", function () {
-        document.querySelector(".header_nav").classList.add("active");
-    });
+toggle.addEventListener("click", function () {
+    document.querySelector(".header_nav").classList.add("active");
+});
+
+
+let nbrOfSlides = document.querySelectorAll(".caroussel__slide").length;
+let currentSlide = 0;
+let dots = document.querySelector(".caroussel__dots");
+//add dots
+for (let i = 0; i < nbrOfSlides; i++) {
+    let dot = document.createElement("div");
+    // add class caroussel__dots__dot and caroussel__dots__dot__active for the active one
+    dot.classList.add("caroussel__dots__dot");
+    if (i === currentSlide) {
+        dot.classList.add("caroussel__dots__dot__active");
+    }
+    dots.appendChild(dot);
+}
+
+setInterval(() => {
+    currentSlide++;
+    if (currentSlide > nbrOfSlides - 1) {
+        currentSlide = 0;
+    }
+    document.querySelector(".caroussel__slide__active").classList.remove("caroussel__slide__active");
+    document.querySelectorAll(".caroussel__slide")[currentSlide].classList.add("caroussel__slide__active");
+    //dots
+    document.querySelector(".caroussel__dots__dot__active").classList.remove("caroussel__dots__dot__active");
+    document.querySelectorAll(".caroussel__dots__dot")[currentSlide].classList.add("caroussel__dots__dot__active");
+}, 5000)
+
+//dots
+
+
+
