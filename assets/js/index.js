58bca8e4517d9d0a4c490f0abaa45ac14b2bc45d
Jawad-Ahmindache <101027749+Jawad-Ahmindache@users.noreply.github.com>
2023-03-05 23:03:39 +0100

    fini

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,48 +1,8 @@
-let close = document.getElementById("header_nav_close");
-let toggle = document.getElementById("header_toggle");
-close.addEventListener("click", function () {
-    document.querySelector(".header_nav").classList.remove("active");
-});
-
-toggle.addEventListener("click", function () {
-    document.querySelector(".header_nav").classList.add("active");
-});
 
 
 $(document).ready(function(){
 
-    // Changer page
-    $(".header_nav_list_item_link").click(function(){
-        $(".header_nav").removeClass("active");
-
-        let link = $(this).attr("data-link");
-        
-        let content = $(document.body).find('.content-box');
-
-        content.html('<p class = "text-light font-bold p-5 text-center">Chargement de '+ $(this).text() +'...</p>');
-        $.ajax({
-            url: "/html/" + link + ".html",
-            type: "GET",
-            crossDomain: true,
-            success: function(data) {
-                content.html(data);
-                switch(link){
-                    case 'contact':
-                        Contact();
-                        break;
-                    case 'support-cours':
-                        SupportCours();
-                        break;
-                    case 'index':
-                        Accueil();
-                        break;
-                }
-            }
-        });
-      
-    });
-
-    $('.header_nav_list_item_link[data-link="index"]').click();
+    navbar();
 });
 
 
@@ -113,62 +73,11 @@ function Contact(){
 }
 
 function SupportCours(){
-    for(let i = 0; i <= 4; i++){
-        
-        $('.support-cours-item').first().clone().appendTo('.support-cours-list');
-
-    }
-    $('.support-cours-item').find('.support-item-arrow').click(function(){
-            let supportItemSub = $(this).parent().parent().find('.support-item-sub');
-            
-            if(supportItemSub.css('display') == 'none'){
-                supportItemSub.css('display','flex');
-                supportItemSub.css('animation','fadeInDown 0.5s ease-in-out');
-                $(this).css('animation','rotateIn 0.2s ease-in-out');
-                $(this).css('transform', 'rotate(90deg)');
-                
-            }
-            else{
-                    supportItemSub.css('display','none');
-                    supportItemSub.css('animation','none');
-                    $(this).css('animation','rotateOut 0.2s ease-in-out');
-                    $(this).css('transform', 'rotate(0deg)');    
-            }
-
-    });
-
+    accordeon();
 
 }
 
 function Accueil(){
-    let caroussel = function(){
-        let nbrOfSlides = document.querySelectorAll(".caroussel__slide").length;
-        let currentSlide = 0;
-        let dots = document.querySelector(".caroussel__dots");
-        //add dots
-        for (let i = 0; i < nbrOfSlides; i++) {
-            let dot = document.createElement("div");
-            // add class caroussel__dots__dot and caroussel__dots__dot__active for the active one
-            dot.classList.add("caroussel__dots__dot");
-            if (i === currentSlide) {
-                dot.classList.add("caroussel__dots__dot__active");
-            }
-            dots.appendChild(dot);
-        }
-        
-        setInterval(() => {
-            currentSlide++;
-            if (currentSlide > nbrOfSlides - 1) {
-                currentSlide = 0;
-            }
-            document.querySelector(".caroussel__slide__active").classList.remove("caroussel__slide__active");
-            document.querySelectorAll(".caroussel__slide")[currentSlide].classList.add("caroussel__slide__active");
-            //dots
-            document.querySelector(".caroussel__dots__dot__active").classList.remove("caroussel__dots__dot__active");
-            document.querySelectorAll(".caroussel__dots__dot")[currentSlide].classList.add("caroussel__dots__dot__active");
-        }, 5000)
-        
-    }
     caroussel();
 }
 
